7623d235f3153880d40228b48ff7f3ab917fde2e
Koichi Kimoto <kkimoto@stanford.edu>
2026-02-15 04:05:30 -0800

    edited drone controller input script

--- DroneCommentator.cs
+++ DroneCommentator.cs
@@ -24,7 +24,7 @@ public class DroneCommentator : MonoBehaviour
     private float idleTimer = 0f;
     private Rigidbody rb;
     private Vector3 lastAngularVelocity; 
-    private bool goalReached = false; // prevents goal spam
+    public bool goalReached = false; // prevents goal spam
     public float jerkThreshold = 15f;   
     private float smoothTurnTimer = 0f;
     public float smoothTurnMinDuration = 1.5f; // how long to hold the turn 
